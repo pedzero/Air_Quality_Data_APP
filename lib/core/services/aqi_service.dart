2b84e46b78c9acaffe3ea2a157f55da887b66a84
pedzero <pedro9669silva@gmail.com>
2024-11-17 19:50:37 -0300

    feat: add air quality index service

---
+++ lib/core/services/aqi_service.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import '../models/aqi.dart';
+
+class AQIService {
+  final String baseUrl = dotenv.env['API_URL'].toString();
+
+  Future<AQI?> fetchAQI(int roomId) async {
+    try {
+      final response =
+          await http.get(Uri.parse('$baseUrl/aqi/?roomId=$roomId'));
+
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+
+        if (jsonData.isNotEmpty) {
+          return AQI.fromJSON(jsonData);
+        } else {
+          return null;
+        }
+      } else {
+        throw Exception('Failed to load city');
+      }
+    } catch (e) {
+      throw Exception('Error fetching city: $e');
+    }
+  }
+}
