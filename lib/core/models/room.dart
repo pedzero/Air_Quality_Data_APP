bfb4cfd4c71fd5d6447f6d8663fb20f6e1d89811
pedzero <pedro9669silva@gmail.com>
2024-11-17 19:41:53 -0300

    feat: improve room model attributes

--- lib/core/models/room.dart
+++ lib/core/models/room.dart
@@ -1,19 +1,40 @@
+import 'package:air_quality_data_app/core/models/AQI.dart';
+import 'package:air_quality_data_app/core/models/parameter.dart';
+
 class Room {
   final String name;
   final int id;
   final int instituteId;
+  final String instituteName;
+  final int cityId;
+  final String cityName;
+  final AQI aqi;
+  final List<Parameter> parameters;
 
   Room({
     required this.name,
     required this.id,
-    required this.instituteId
+    required this.instituteId,
+    required this.instituteName,
+    required this.cityId,
+    required this.cityName,
+    required this.aqi,
+    required this.parameters,
   });
-  
+
   factory Room.fromJSON(Map<String, dynamic> json) {
     return Room(
-      name: json['name'],
-      id: json['id'],
-      instituteId: json['institute_id']
+      name: json['name'] ?? '',
+      id: json['id'] ?? 0,
+      instituteId: json['institute_id'] ?? 0,
+      instituteName: json['institute_name'] ?? '',
+      cityId: json['city_id'] ?? 0,
+      cityName: json['city_name'] ?? '',
+      aqi: AQI.fromJSON(json['aqi'] ?? {}), 
+      parameters: (json['parameters'] as List<dynamic>?)
+              ?.map((param) => Parameter.fromJSON(param))
+              .toList() ??
+          [], 
     );
   }
 }
